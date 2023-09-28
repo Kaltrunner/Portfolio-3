1a3b7ae7fdde0eb72da8a6aa652a9070b71bb2de
Erik Russell <erikrussells@gmail.com>
2023-09-28 18:21:34 -0400

    working on the about and work pages

--- src/assets/About/About.jsx
+++ src/assets/About/About.jsx
@@ -1,8 +1,81 @@
 import "./about.css";
+import React, { useEffect } from "react";
 import CompNav from "../CompNav/CompNav";
 import Mouse from "../Mouse";
+import gsap from "gsap";
 
 function About() {
+  useEffect(() => {
+    const hero = document.querySelector(".about-body-container");
+    const heroTitles = hero.querySelectorAll(".about-row-text > h1");
+    const subHeroTitles = hero.querySelectorAll(".about-lorem-div > p");
+    const heroSeparator = hero.querySelectorAll(".about-row-separator");
+
+    const initHero = () => {
+      gsap.set(heroTitles, { y: "101%" });
+      gsap.set(subHeroTitles, { y: "60%" });
+      gsap.set(heroSeparator, { width: 0 });
+    };
+
+    const showHero = () => {
+      gsap
+        .timeline({ defaults: { ease: "expo-out" } })
+        .to(
+          heroTitles,
+          {
+            duration: 1,
+            y: 0,
+            stagger: 0.055,
+          },
+          0
+        )
+        .fromTo(
+          subHeroTitles, // Select the subHeroTitles
+          {
+            opacity: 0, // Start from 0 opacity
+          },
+          {
+            duration: 1,
+            opacity: 1, // Animate opacity to 1 for full visibility
+            y: 0,
+            stagger: 0.055,
+          },
+          0
+        )
+        .to(
+          heroSeparator,
+          {
+            duration: 1,
+            width: "100%",
+            stagger: 0.095,
+          },
+          0
+        )
+        .fromTo(
+          ".cell",
+          {
+            height: "0",
+            scale: 0.5,
+          },
+          {
+            duration: 1,
+            height: "100%",
+            scale: 1,
+            stagger: 0.025,
+            ease: "expo.inOut",
+          },
+          0.5
+        );
+    };
+
+    initHero();
+    showHero();
+  }, []); // Empty dependency array ensures this code runs only once after initial render
+
+  window.onload = () => {
+    initHero();
+  };
+
   return (
     <>
       <Mouse />
@@ -11,7 +84,7 @@ function About() {
         <div className="about-body-div">
           <div className="about-row">
             <div className="about-row-text">
-              <h1>ABOUT ME!</h1>
+              <h1>Hello</h1>
               <p className="nav-text" id="nav-under-line">
                 <a
                   className="pseudo-text-effect"
@@ -46,21 +119,17 @@ function About() {
                 </a>
               </p>
             </div>
-            <div className="hero-row-separator"></div>
+            <div className="about-row-separator"></div>
           </div>
-          <div className="lorem-div">
+          <div className="about-lorem-div">
             <p className="lorem">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus
-              perspiciatis, exercitationem voluptatem veniam iste, aliquid
-              officia obcaecati tempora illum accusantium quasi cupiditate,
-              dolores distinctio beatae. Lorem ipsum dolor sit amet consectetur,
-              adipisicing elit. Possimus perspiciatis, exercitationem voluptatem
-              veniam iste, aliquid officia obcaecati tempora illum accusantium
-              quasi cupiditate, dolores distinctio beatae. Lorem ipsum dolor sit
-              amet consectetur, adipisicing elit. Possimus perspiciatis,
-              exercitationem voluptatem veniam iste, aliquid officia obcaecati
-              tempora illum accusantium quasi cupiditate, dolores distinctio
-              beatae.
+              I'm a Flatiron School graduate and front-end engineer with a
+              background in screen printing. My passion lies in merging design
+              and technology to create user-friendly interfaces that prioritize
+              usability. I draw from best practices to ensure efficient and
+              enjoyable navigation experiences. My goal is to bridge the gap
+              between art and technology, crafting visually appealing and
+              functional solutions.
             </p>
           </div>
         </div>
--- src/assets/Work/Work.jsx
+++ src/assets/Work/Work.jsx
@@ -1,8 +1,77 @@
+import React, { useEffect } from "react";
 import "./work.css";
 import CompNav from "../CompNav/CompNav";
 import Mouse from "../Mouse";
+import gsap from "gsap";
 
 function Work() {
+  useEffect(() => {
+    const hero = document.querySelector(".work-body-container");
+    const heroTitles = hero.querySelectorAll(".work-row-text > h1");
+    const subHeroTitles = hero.querySelectorAll(".sub-work-row-text > p");
+    const heroSeparator = hero.querySelectorAll(".work-row-separator");
+
+    const initHero = () => {
+      gsap.set(heroTitles, { y: "101%" });
+      gsap.set(subHeroTitles, { y: "101%" });
+      gsap.set(heroSeparator, { width: 0 });
+    };
+
+    const showHero = () => {
+      gsap
+        .timeline({ defaults: { ease: "expo-out" } })
+        .to(
+          heroTitles,
+          {
+            duration: 1,
+            y: 0,
+            stagger: 0.055,
+          },
+          0
+        )
+        .fromTo(
+          subHeroTitles, // Select the subHeroTitles
+          {
+            opacity: 0, // Start from 0 opacity
+          },
+          {
+            duration: 0.75,
+            opacity: 1, // Animate opacity to 1 for full visibility
+            y: 0,
+            stagger: 0.055,
+          },
+          0
+        )
+        .to(
+          heroSeparator,
+          {
+            duration: 1,
+            width: "100%",
+            stagger: 0.095,
+          },
+          0
+        )
+        .fromTo(
+          ".cell",
+          {
+            height: "0",
+            scale: 0.5,
+          },
+          {
+            duration: 1,
+            height: "100%",
+            scale: 1,
+            stagger: 0.025,
+            ease: "expo.inOut",
+          },
+          0.5
+        );
+    };
+
+    initHero();
+    showHero();
+  }, []); // Empty dependency array ensures this code runs only once after initial render
+
   return (
     <>
       <Mouse />
@@ -11,20 +80,153 @@ function Work() {
         <div className="work-body-div">
           <div className="work-row">
             <div className="work-row-text">
-              <h1>WORK!</h1>
+              <h1>Select Work</h1>
               <h1 id="est">2022 – 23</h1>
             </div>
-            <div className="hero-row-separator"></div>
+            <div className="work-row-separator"></div>
           </div>
-          <div className="lorem-div">
-            <p className="lorem">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta,
-              numquam deleniti. Ipsa optio tenetur, obcaecati, quam architecto
-              quidem natus, quod aspernatur laudantium ab veniam fuga quia
-              officiis numquam maiores eaque? Veniam dolore culpa dolor, iste at
-              voluptatem, debitis vitae sit illo consequatur eos alias hic in
-              ipsum delectus non consectetur!
-            </p>
+          <div className="sub-work-row">
+            <div className="sub-work-row-text" id="sub-work-row-text-header">
+              <p>Projects</p>
+              <p>Year</p>
+            </div>
+            <div className="hidden-rudy">
+              <div className="sub-work-row-text" id="row-1">
+                <p>.Rudy</p>
+                <p>2023</p>
+                <span className="hidden-span">
+                  <a
+                    href="https://rudy.onrender.com/"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <img
+                      className="hidden-img"
+                      src="Screenshot 2023-09-28 at 6.19.22 PM.png"
+                      alt=""
+                    />
+                  </a>
+                </span>
+              </div>
+            </div>
+            <div className="hidden-muse">
+              <div className="sub-work-row-text" id="row-1">
+                <p>Nútímasafn</p>
+                <p>2023</p>
+                <span className="hidden-span">
+                  <a
+                    href="https://nutimasafn.onrender.com/"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <img
+                      className="hidden-img"
+                      src="Screenshot 2023-09-28 at 6.15.14 PM.png"
+                      alt=""
+                    />
+                  </a>
+                </span>
+              </div>
+            </div>
+            <div className="hidden-sid">
+              <div className="sub-work-row-text" id="row-1">
+                <p>Sid Design System</p>
+                <p>2023</p>
+                <span className="hidden-span">
+                  <a
+                    href="https://design-system-sid.onrender.com/"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <img
+                      className="hidden-img"
+                      src="Screenshot 2023-09-28 at 5.10.54 PM.png"
+                      alt=""
+                    />
+                  </a>
+                </span>
+              </div>
+            </div>
+            <div className="hidden-kalt">
+              <div className="sub-work-row-text" id="row-1">
+                <p>KALTRUNNER</p>
+                <p>2023</p>
+                <span className="hidden-span">
+                  <a
+                    href="https://kaltrunner.com/"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <img
+                      className="hidden-img"
+                      src="Screenshot 2023-09-28 at 4.57.18 PM.png"
+                      alt=""
+                    />
+                  </a>
+                </span>
+              </div>
+            </div>
+            <div className="sub-work-row-text" id="sub-work-row-text-header">
+              <p>Experimental</p>
+            </div>
+            <div className="hidden-green">
+              <div className="sub-work-row-text" id="row-1">
+                <p>The Green Square</p>
+                <p>2022</p>
+                <span className="hidden-span">
+                  <a
+                    href="https://a-green-square-project.onrender.com/"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <img
+                      className="hidden-img"
+                      src="Screenshot-the-green-square.png"
+                      alt=""
+                    />
+                  </a>
+                </span>
+              </div>
+            </div>
+            <div className="hidden-new">
+              <div className="sub-work-row-text" id="row-1">
+                <p>New Matter</p>
+                <p>2023</p>
+                <span className="hidden-span">
+                  <a
+                    href="https://new-matter.onrender.com/"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <img
+                      className="hidden-img"
+                      src="Screenshot 2023-09-01 at 2.29.17 PM.png"
+                      alt=""
+                    />
+                  </a>
+                </span>
+              </div>
+            </div>
+            <div className="hidden-helvetica">
+              <div className="sub-work-row-text" id="row-1">
+                <p>Helvetica</p>
+                {/* <p>Experimental</p> */}
+                <p>2023</p>
+                <span className="hidden-span">
+                  <a
+                    href="https://helvetica.onrender.com/"
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    <img
+                      className="hidden-img"
+                      src="Screenshot-font-app.png"
+                      alt=""
+                    />
+                  </a>
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
