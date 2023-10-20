67d13755bb3b91052dd4cf165a57b1d428e39539
Erik Russell <erikrussells@gmail.com>
2023-10-20 00:25:54 -0400

    still making style updates

--- src/assets/Splash/Splash.jsx
+++ src/assets/Splash/Splash.jsx
@@ -72,6 +72,8 @@ function Splash() {
     showHero();
   }, []); // Empty dependency array ensures this code runs only once after initial render
 
+  // const text = " • N 40.7305°, W 73.9515° ";
+
   return (
     <>
       <Mouse />
@@ -88,14 +90,28 @@ function Splash() {
           <div className="hero-row">
             <div className="hero-row-text">
               <h1>Front–End</h1>
-              <p className="coordinates">• N 40.7305°, W 73.9515°</p>
+              {/* <div className="circle">
+                <div className="text"> */}
+              <p className="coordinates">
+                • N 40.7305°, W 73.9515°
+                {/* {text.split("").map((char, i) => (
+                      <span
+                        key={i}
+                        style={{ transform: `rotate(${i * 8.3}deg)` }}
+                      >
+                        {char}
+                      </span>
+                    ))} */}
+              </p>
+              {/* </div>
+              </div> */}
             </div>
             <div className="hero-row-separator"></div>
           </div>
           <div className="hero-row">
             <div className="hero-row-text">
-              <h1>Developer</h1>
-              <p className="coordinates">© 2023</p>
+              <h1>Software Engineer</h1>
+              {/* <p className="coordinates">© 2023</p> */}
             </div>
             <div className="hero-row-separator"></div>
           </div>
